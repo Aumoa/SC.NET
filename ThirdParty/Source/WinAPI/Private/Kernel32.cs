eab9dae9238cb76289bcaf0304321b61422e9eaa
liberty <liberty@clover.games>
2021-04-18 23:43:08 +0900

    HandleBase 클래스 추가

--- ThirdParty/Source/WinAPI/Private/Kernel32.cs
+++ ThirdParty/Source/WinAPI/Private/Kernel32.cs
@@ -1,4 +1,5 @@
-﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+﻿
+// Copyright 2020-2021 Aumoa.lib. All right reserved.
 
 using System;
 using System.Runtime.InteropServices;
@@ -32,5 +33,17 @@ namespace SC.ThirdParty.WinAPI
         /// </returns>
         [DllImport("Kernel32.dll")]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        /// <summary>
+        /// Closes an open object handle.
+        /// </summary>
+        /// <param name="hObject"> A valid handle to an open object. </param>
+        /// <returns>
+        /// <para> If the function succeeds, the return value is nonzero. </para>
+        /// <para> If the function fails, the return value is zero. To get extended error information, call <see cref="GetLastError"/>. </para>
+        /// <para> If the application is running under a debugger, the function will throw an exception if it receives either a handle value that is not valid or a pseudo-handle value. This can happen if you close a handle twice, or if you call CloseHandle on a handle returned by the FindFirstFile function instead of calling the FindClose function. </para>
+        /// </returns>
+        [DllImport("Kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
     }
 }
---
+++ ThirdParty/Source/WinAPI/Public/GeneralHandle.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2020-2021 Aumoa.lib. All right reserved.
+
+using System;
+
+using static SC.ThirdParty.WinAPI.Kernel32;
+
+namespace SC.ThirdParty.WinAPI
+{
+    /// <summary>
+    /// Windows에서 제공하는 기본 핸들을 사용합니다.
+    /// </summary>
+    public class GeneralHandle : IPlatformHandle
+    {
+        IntPtr _handle;
+
+        /// <summary>
+        /// 개체를 초기화합니다.
+        /// </summary>
+        /// <param name="handle"> 핸들 값을 전달합니다. </param>
+        public GeneralHandle(IntPtr handle)
+        {
+            _handle = handle;
+        }
+
+        /// <summary>
+        /// 핸들을 닫고 개체 사용을 종료합니다.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            Close();
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 핸들을 닫습니다.
+        /// </summary>
+        public virtual void Close()
+        {
+            if (_handle != IntPtr.Zero)
+            {
+                CloseHandle(_handle);
+                _handle = IntPtr.Zero;
+            }
+        }
+
+        /// <summary>
+        /// 참조된 핸들을 가져옵니다.
+        /// </summary>
+        /// <returns> 값이 반환됩니다. </returns>
+        public virtual IntPtr GetHandle()
+        {
+            return _handle;
+        }
+    }
+}
